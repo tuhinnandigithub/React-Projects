1232b8f3a913c8d927521eb320eddc33c17f3fe0
Tuhin Nandi <97544624+tuhinnandigithub@users.noreply.github.com>
2022-04-25 11:08:33 +0530

    Whatsapp_clone_Changes

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCfxMrYpNmWwSOyF5U1IyyrKsAoe90IHAg",
+    authDomain: "whatsapp-clone-6f3b1.firebaseapp.com",
+    projectId: "whatsapp-clone-6f3b1",
+    storageBucket: "whatsapp-clone-6f3b1.appspot.com",
+    messagingSenderId: "45691621150",
+    appId: "1:45691621150:web:02449d25a19b1b9037c30f",
+    measurementId: "G-148FYHV51T"
+  };
