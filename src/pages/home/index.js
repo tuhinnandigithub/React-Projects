5ae020d52560f46132b0b4ed763dfc84cc6a4e60
Tuhin <tuhinnandi9@gmail.com>
2022-05-01 19:15:34 +0530

    Expense Tracker App basic design code

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+  <div className='home'>
+    <div>Topfold</div> 
+    <div>ExpenseList</div>     
+  </div>
+  );
+}
+
+export default Home;
