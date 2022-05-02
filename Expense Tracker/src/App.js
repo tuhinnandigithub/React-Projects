448355eea2269634de059ccad2e9df0a8e81742c
Tuhin Nandi <97544624+tuhinnandigithub@users.noreply.github.com>
2022-05-02 23:43:29 +0530

    More changes to UI

--- Expense Tracker/src/App.js
+++ Expense Tracker/src/App.js
@@ -1,14 +1,19 @@
 import Home from "./pages/home";
 import Header from "./components/header";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddExpense from "./pages/add-expense";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-      <div>Footer</div>
-    </div>
-  )
-};
+    <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/add-expense" element={<AddExpense/>}/>
+        </Routes>
+        <div>Footer</div>
+    </Router>
+  );
+}
 
 export default App;
