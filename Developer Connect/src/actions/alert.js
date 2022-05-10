fc739f84c039e3b7cb8ebe4aeb1d84a2ee5e3eb0
Tuhin Nandi <97544624+tuhinnandigithub@users.noreply.github.com>
2022-05-10 17:47:23 +0530

    Auth Reducers and Register Actions

--- Developer Connect/src/actions/alert.js
+++ Developer Connect/src/actions/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from "./types";
 import {v4 as uuid} from 'uuid'
 
 export const setAlert = (message, alertType, timeOut = 5000) => dispatch => {
-    const id = uuid.v4();
+    const id = uuid();
     dispatch({
         type : SET_ALERT,
         payload : {message, alertType, id}
--- Developer Connect/src/components/auth/Register.js
+++ Developer Connect/src/components/auth/Register.js
@@ -2,10 +2,11 @@ import React, { Fragment,useState } from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import {register} from '../../actions/auth'
 import axios from 'axios';
 import PropTypes from 'prop-types';
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
     const [formData, setFormData] = useState({
         name:"",
         email: "",
@@ -20,7 +21,7 @@ const Register = ({setAlert}) => {
         if(password !== password2){
             setAlert("Password does not match", "danger");
         }else{
-            console.log("SUCCESS");
+            register({name,email,password})
         //    const newUser = {
         //         name,
         //         email,
@@ -90,7 +91,8 @@ const Register = ({setAlert}) => {
 };
 
 Register.propTypes = {
-  setAlert : PropTypes.func.isRequired
-};
+  setAlert : PropTypes.func.isRequired,
+  register : PropTypes.func.isRequired,
+}
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert,register})(Register);
